82d4aa6738d2ad6d5adb4ebd5053bd372c50c532
Alexis <alexandrescu.alexandra.maria@gmail.com>
2023-04-24 12:54:41 +0300

    adds alert if the rout is not compatible with the current role of the user

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -5,18 +5,40 @@ import colors from '../consts/Colors';
 import { useAppDispatch, useAppSelector } from '../redux/store';
 import { connectAction, disconnectAction, getUserRoleAction } from '../redux/actions';
 import { selectCurrentUserRole, selectUsereSessionState } from '../redux/reducers/user-state';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useSelect } from '@mui/base';
 
 const appConfig = new AppConfig(['store_write', 'publish_data']);
+
+type UserAloowedRoutes = 'normalUser' | 'pendingMiner' | 'miner' | 'viewer' | '/';
 
 interface ConnectWalletProps {
   currentTheme: string;
 }
 
 const ConnectWallet = ({ currentTheme }: ConnectWalletProps) => {
+  const [currentPathName, setCurrentPathName] = useState<string | UserAloowedRoutes>('');
   const userSession = useAppSelector(selectUsereSessionState);
   const dispatch = useAppDispatch();
 
   const currentRole = useAppSelector(selectCurrentUserRole);
+  const location = useLocation();
+  console.log(location);
+  console.log('___', location.pathname.substring(1));
+  const controlAccessRoutes = () => {
+    setCurrentPathName(location.pathname);
+    if (currentPathName !== '/') {
+      if (currentPathName.substring(1).toLowerCase() !== currentRole.toLowerCase()) {
+        return alert('Seems like you got lost, click here to go back to the main page');
+        console.log('Seems like you got lost, click here to go back to the main page');
+      }
+    }
+  };
+
+  useEffect(() => {
+    controlAccessRoutes();
+  }, [location.pathname]);
 
   const disconnect = () => {
     dispatch(disconnectAction());
