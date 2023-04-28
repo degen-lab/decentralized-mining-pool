76cd2759c5880d725047a0699b734429bafc745a
Alexis <alexandrescu.alexandra.maria@gmail.com>
2023-04-28 11:47:36 +0300

    update profile component as mentioned in the pull request

--- src/components/appMenuSections/profile/CommonInfoProfile.tsx
+++ src/components/appMenuSections/profile/CommonInfoProfile.tsx
@@ -1,9 +1,13 @@
+import { selectCurrentUserRole } from '../../../redux/reducers/user-state';
+import { useAppSelector } from '../../../redux/store';
+
 const CommonInfoProfile = () => {
+  const currentRole = useAppSelector(selectCurrentUserRole);
   return (
     <ul>
       <li>display connected wallet</li>
       <li>option to change wallet</li>
-      <li>current role: </li>
+      <li>current role: {currentRole}</li>
       <li>link to explorer</li>
     </ul>
   );
--- src/components/appMenuSections/profile/Profile.tsx
+++ src/components/appMenuSections/profile/Profile.tsx
@@ -7,15 +7,16 @@ import WaitingMinerProfile from './WaitingMinerProfile';
 
 const Profile = () => {
   const currentRole: UserRole = useAppSelector(selectCurrentUserRole);
-  return (
-    <div>
-      <h2>Profile</h2>
-      {currentRole !== 'Miner' && <CommonInfoProfile />}
-      {currentRole === 'WaitingMiner' && <WaitingMinerProfile />}
-      {currentRole === 'PendingMiner' && <PendingMinerProfile />}
-      {currentRole === 'Miner' && <MinerProfile />}
-    </div>
-  );
+
+  const profileMapping: Record<UserRole, React.ReactElement> = {
+    Viewer: <CommonInfoProfile />,
+    NormalUser: <CommonInfoProfile />,
+    WaitingMiner: <WaitingMinerProfile />,
+    PendingMiner: <PendingMinerProfile />,
+    Miner: <MinerProfile />,
+  };
+
+  return profileMapping[currentRole];
 };
 
 export default Profile;
